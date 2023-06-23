30363148e79cc58127be4c295b91e236bce189f3
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-06-23 00:08:55 -0500

    Display pokémon name and number

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
 import { NextPage, GetStaticProps } from 'next';
 import { Layout } from '@/components/layouts';
 import { pokeApi } from '@/api';
-import { PokemonListResponse } from '@/interfaces';
+import { PokemonListResponse, SmallPokemon } from '@/interfaces';
 
-const Home: NextPage = (props) => {
-  console.log(props);
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title="Pokemon">
       <ul>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
+        {pokemons.map(({ id, name }) => (
+          <li key={id}>
+            #{id} - {name}
+          </li>
+        ))}
       </ul>
     </Layout>
   );
@@ -24,11 +23,22 @@ const Home: NextPage = (props) => {
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
-  console.log(data);
+
+  //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/id.svg
+  const pokemons: SmallPokemon[] = data.results.map<SmallPokemon>(
+    (pokemon, i): SmallPokemon => ({
+      id: i + 1,
+      name: pokemon.name,
+      url: pokemon.url,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+        i + 1
+      }.svg`,
+    })
+  );
 
   return {
     props: {
-      pokemons: data.results,
+      pokemons,
     },
   };
 };
