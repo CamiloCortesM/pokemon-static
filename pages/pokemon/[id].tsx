978d92db788bb2b84661ecd1943c14fcd111ffc7
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-06-23 00:47:38 -0500

    Next - getStaticPaths

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,18 +1,51 @@
 import { Layout } from '@/components/layouts';
+import { GetStaticProps, NextPage, GetStaticPaths } from 'next';
 import { useRouter } from 'next/router';
 
 interface Props {
-  pokemon: any;
+  // pokemon: any;
+  id: string;
+  name: string;
 }
 
-const PokemonPage = () => {
+const PokemonPage: NextPage<Props> = ({ id, name }) => {
   const router = useRouter();
 
   return (
     <Layout title="Algun pokemon">
-      <h1>Hola mundo</h1>
+      <h1>
+        {id} - {name}
+      </h1>
     </Layout>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  return {
+    paths: [
+      {
+        params: { id: '1' },
+      },
+      {
+        params: { id: '2' },
+      },
+      {
+        params: { id: '3' },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  // const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+  return {
+    props: {
+      id: 1,
+      name: 'bulbasaur',
+    },
+  };
 };
 
 export default PokemonPage;
