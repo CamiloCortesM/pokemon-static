ce0bd9da82486886a044b8b5595e2ed3f7223e95
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-06-23 20:31:48 -0500

    Build the list of favorite Pokémons

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,7 +1,8 @@
+import { useEffect, useState } from 'react';
+import { localFavorites } from '@/utils';
 import { Layout } from '@/components/layouts';
 import { NoFavorites } from '@/components/ui';
-import { localFavorites } from '@/utils';
-import { useEffect, useState } from 'react';
+import { Card, Grid } from '@nextui-org/react';
 
 const FavoritesPage = () => {
   const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
@@ -12,7 +13,23 @@ const FavoritesPage = () => {
 
   return (
     <Layout title="Pokemons - Favorites">
-      <NoFavorites />
+      {favoritePokemons.length === 0 ? (
+        <NoFavorites />
+      ) : (
+        <Grid.Container gap={2} direction="row" justify="flex-start">
+          {favoritePokemons.map((id) => (
+            <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+              <Card isHoverable isPressable css={{ padding: 10 }}>
+                <Card.Image
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                  width={'100%'}
+                  height={140}
+                />
+              </Card>
+            </Grid>
+          ))}
+        </Grid.Container>
+      )}
     </Layout>
   );
 };
